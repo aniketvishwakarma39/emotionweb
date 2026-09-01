4c2970acf4c29cd6f0c0503fc3da35429fa65372
unknown <aniketvishwa39@gmail.com>
2026-09-02 00:28:42 +0530

    >

--- anniversary.py
+++ anniversary.py
@@ -1,17 +1,18 @@
 from emotionweb import Surprise
 
 s=Surprise(
-    type="birthday",
-    name="Aniket Vishwakarma",
-    message="happy birthday sir",
+    type="anniversary",
+    name="aniket",
+    message ="happy anniversary honey",
     images=[
-        "images/avi.jpeg",
+         "images/avi.jpeg",
         "images/travel.avif",
     ],
     caption=[
         "i love you honey",
         "aniket is aniket",
     ]
-    
+
+
 )
 s.generate()
